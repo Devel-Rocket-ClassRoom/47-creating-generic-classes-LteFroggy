38174db5f574a3998f87a94d70eff80c63d4149c
hojong <33508056+LteFroggy@users.noreply.github.com>
2026-03-12 18:10:41 +0900

    GenericConverter 완료

---
+++ GenericConverter/Converter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Converter<TInput, TOutput> {
+    private Func<TInput, TOutput> _converter;
+
+    public Converter(Func<TInput, TOutput> converter) {
+        _converter = converter;
+    }
+
+    public TOutput Convert(TInput target) {
+        return _converter(target);
+    }
+
+    public TOutput[] ConvertAll(TInput[] arr) {
+        TOutput[] result = new TOutput[arr.Length];
+        for (int i = 0; i < arr.Length; i++) {
+            result[i] = _converter(arr[i]);
+        }
+
+        return result;
+    }
+}
--- GenericConverter/Program.cs
+++ GenericConverter/Program.cs
@@ -1,4 +1,51 @@
 using System;
+using System.Security.Cryptography;
 
-// README.md를 읽고 아래에 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+Func<string, int> stoiConvert = (str) => str.Length;
+Func<int, string> itosConvert = (val) => $"{val}번";
+Func<double, int> dtoiConvert = (dVal) => (int)dVal;
+
+// string -> int 변환
+Converter<string, int> stoiConverter = new Converter<string, int>(stoiConvert);
+string[] strArr = new string[] { "Hello", "World", "hi" };
+int[] stoiConvertedArr = stoiConverter.ConvertAll(strArr);
+
+// int -> string 변환
+Converter<int, string> itosConverter = new Converter<int, string>(itosConvert);
+int[] intArr = new int[] { 1, 2, 3 };
+string[] itosConvertedArr = itosConverter.ConvertAll(intArr);
+
+// double -> int 변환
+Converter<double, int> dtoiConverter = new Converter<double, int>(dtoiConvert);
+double[] doubleArr = new double[] { 3.2, 1.0, 9.7 };
+int[] dtoiConvertedArr = dtoiConverter.ConvertAll(doubleArr);
+
+// string -> int 출력
+Console.WriteLine($"=== 문자열 -> 길이 변환 ===");
+Console.WriteLine($"Hello -> {stoiConverter.Convert("Hello")}");
+Console.Write($"전체 변환 :");
+for (int i = 0; i < stoiConvertedArr.Length; i++) {
+Console.Write($"{(i != 0 ? ", " : " ")}" +
+              $"{stoiConvertedArr[i]}");}
+Console.WriteLine();
+Console.WriteLine();
+
+// int -> string 출력
+Console.WriteLine($"=== 정수 -> 문자열 변환 ===");
+Console.WriteLine($"1 -> {itosConverter.Convert(1)}");
+Console.Write($"전체 변환 :");
+for (int i = 0; i < itosConvertedArr.Length; i++) {
+Console.Write($"{(i != 0 ? ", " : " ")}" +
+              $"{itosConvertedArr[i]}");}
+Console.WriteLine();
+Console.WriteLine();
+
+// dobble -> int 출력
+Console.WriteLine($"=== 실수 -> 정수 변환 ===");
+Console.WriteLine($"1 -> {dtoiConverter.Convert(1)}");
+Console.Write($"전체 변환 :");
+for (int i = 0; i < dtoiConvertedArr.Length; i++) {
+Console.Write($"{(i != 0 ? ", " : " ")}" +
+              $"{dtoiConvertedArr[i]}");}
+Console.WriteLine();
+Console.WriteLine();
