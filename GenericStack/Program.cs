d73b0a6d81534bf52814eacbc4f2dcd2786c5ca4
hojong <33508056+LteFroggy@users.noreply.github.com>
2026-03-13 09:48:34 +0900

    GenericStack -> GenericQueue 변경 및 완료

--- GenericStack/Program.cs
+++ GenericStack/Program.cs
@@ -1,4 +1,27 @@
 using System;
 
-// README.md를 읽고 아래에 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+SimpleQueue<int> queInt = new SimpleQueue<int>(3);
+queInt.Enqueue(10);
+queInt.Enqueue(20);
+queInt.Enqueue(30);
+Console.WriteLine($"=== int 큐 (용량 : 3) ===");
+Console.WriteLine($"Enqueue : 10, 20, 30");
+Console.WriteLine($"Count : {queInt.Count}, IsFull : {queInt.IsFull}");
+queInt.Enqueue(40);
+Console.WriteLine($"Peak : {queInt.Peek()}");
+Console.WriteLine($"Dequeue : {queInt.Dequeue()}");
+Console.WriteLine($"Dequeue : {queInt.Dequeue()}");
+Console.WriteLine($"Count : {queInt.Count}, IsFull : {queInt.IsFull}");
+Console.WriteLine();
+
+SimpleQueue<string> queStr = new SimpleQueue<string>(2);
+queStr.Enqueue("Hello");
+queStr.Enqueue("World");
+Console.WriteLine($"=== string 큐 (용량 : 2)");
+Console.WriteLine($"Enqueue : Hello, World");
+Console.WriteLine($"Dequeue : {queStr.Dequeue()}");
+Console.WriteLine($"Dequeue : {queStr.Dequeue()}");
+Console.WriteLine($"Dequeue : {queStr.Dequeue()}");
+Console.WriteLine($"IsEmpty : {queStr.IsEmpty}");
+Console.WriteLine();
+
---
+++ GenericStack/SimpleQueue.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class SimpleQueue<T> {
+    private T[] _items;
+    private int _front;
+    private int _back;
+    private int _size;
+    private int _capacity;
+
+    public int Count => _size;
+    public bool IsFull => _size == _capacity;
+    public bool IsEmpty => _size == 0;
+
+    public SimpleQueue(int capacity) {
+        _items = new T[capacity];
+        _front = 0;
+        _back = 0;
+        _capacity = capacity;
+        _size = 0;
+    }
+    public void Enqueue(T item) {
+        if(IsFull) {
+            Console.WriteLine($"큐가 가득 찼습니다.");
+            return;
+        }
+        _items[(_back++) % _capacity] = item;
+        _size++;
+    }
+
+    public T Dequeue() {
+        if (IsEmpty) {
+            Console.WriteLine($"큐가 비어있습니다.");
+            return default(T);
+        }
+        _size--;
+        return _items[(_front++) % _capacity];
+    }
+
+    public T Peek() {
+        if (IsEmpty) {
+            Console.WriteLine($"큐가 비어있습니다.");
+            return default(T);
+        }
+
+        return _items[(_back - 1) % _capacity];
+    }
+}
